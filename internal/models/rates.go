94f292ecbcca21abe3b4e61c23f86b518d978d14
Dmitry Molchanov <11kilos@mail.ru>
2025-02-01 03:22:23 +0300

    add controller + models

---
+++ internal/models/rates.go
@@ -0,0 +1,7 @@
+package models
+
+type Rates struct {
+	Ask       float64
+	Bid       float64
+	Timestamp int64
+}
---
+++ internal/modules/controller/rates_controller.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"context"
+	pb "get-rates-usdt-grpc-service/protogen/golang/get-rates"
+)
+
+type RatesController struct {
+	pb.UnimplementedRatesServiceServer
+	service service.RatesService
+}
+
+func NewRatesController(s service.RatesService) *RatesController {
+	return &RatesController(service: s)
+}
+
+func (c *RatesController) GetRates(ctx context.Context, req *pb.GetRatesRequest) (*pb.GetRatesResponse, error) {
+	rate, err := c.service.GetCurrentRate(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetRatesResponse{
+		Ask: rate.Ask,
+		Bid: rate.Bid,
+		Timestamp: rate.Timestamp,
+	}, nil
+}
+
+func (c *RatesController) HealthCheck(ctx context.Context, req *pb.HealthCheckRequest) (*pb.HealthCheckResponse, error) {
+	return &pb.HealthCheckResponse{
+		Status: pb.HealthCheckResponse_SERVING,
+	}, nil
+}
